287d6376afac762ccdcec4d8c1f8ee6d14535eae
Gisele Rodriguez <105676292+GigiR12@users.noreply.github.com>
2022-06-24 11:53:40 -0300

    subo escrito

---
+++ Escrito_Grafica/Escrito_Grafica/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Escrito_Grafica
+{
+    class Class1
+    {
+        ArrayList Lista = new ArrayList();
+        public class Deportista
+        {
+            public int cI { get; set; }
+            public string nombre { get; set; }
+            public string fechaNac { get; set; }
+            public int horasEntren { get; set; }
+
+            public Deportista() { }
+            public Deportista(int cI, string nombre, string fechaNac, int horasEntren)
+            {
+                this.cI = cI;
+                this.nombre = nombre;
+                this.fechaNac = fechaNac;
+                this.horasEntren = horasEntren;
+            }
+        }
+
+        public class Amateur:Deportista
+        {
+            private int añosPract { get; set; }
+            public Amateur() { }
+            public Amateur(int cI, string nombre, string fechaNac, int horasEntren, int añosPract):base(cI,nombre,fechaNac,horasEntren)
+            {
+                this.añosPract = añosPract;
+            }
+
+            public int CostoMenA()
+            {
+                int costo;
+                costo = horasEntren * 300;
+                return costo;
+            }
+
+            public string MostrarAmat()
+            {
+               return ("La cedula del deportista es: " + cI + ",el nombre es: " + nombre + ", la fecha de nacimiento es: " + fechaNac + ",las horas de entrenamiento son: " + horasEntren + ", los años de practica son: " + añosPract + " y el costo mensual es: " + CostoMenA());
+            }
+        }
+        public class Profesional : Deportista
+        { 
+            private int sueldo { get; set; }
+            public Profesional() { }
+            public Profesional(int cI, string nombre, string fechaNac, int horasEntren, int sueldo) : base(cI, nombre, fechaNac, horasEntren)
+            {
+                this.sueldo = sueldo;
+            }
+
+            public int CostoMenP()
+            {
+                int costo;
+                costo = (horasEntren * 30000) + sueldo;
+                return costo;
+            }
+            
+            public string MostrarProf()
+            {
+                return ("La cedula del deportista es: " + cI + ",el nombre es: " + nombre + ", la fecha de nacimiento es: " + fechaNac + ",las horas de entrenamiento son: " + horasEntren + ", el sueldo es: " + sueldo + " y el costo mensual es: " +CostoMenP());
+            }
+
+           
+        }
+    }
+}
---
+++ Escrito_Grafica/Escrito_Grafica/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Escrito_Grafica
+{
+    public partial class Form1 : Form
+    {
+        ArrayList Lista = new ArrayList();
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+ 
+
+        }
+            
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int cedu = Convert.ToInt32(textBox1.Text);
+            int horas = Convert.ToInt32(textBox4.Text);
+            int años = Convert.ToInt32(textBox5.Text);
+            Class1.Amateur amateur = new Class1.Amateur(cedu,textBox2.Text,textBox3.Text,horas,años);
+            Lista.Add(amateur.MostrarAmat());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int cedu = Convert.ToInt32(textBox6.Text);
+            int horas = Convert.ToInt32(textBox9.Text);
+            int sueldo = Convert.ToInt32(textBox10.Text);
+            Class1.Profesional profesional = new Class1.Profesional(cedu, textBox7.Text, textBox8.Text, horas, sueldo);
+            Lista.Add(profesional.MostrarProf());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int cedu = Convert.ToInt32(textBox1.Text);
+            int horas = Convert.ToInt32(textBox4.Text);
+            int años = Convert.ToInt32(textBox5.Text);
+            Class1.Amateur amateur = new Class1.Amateur(cedu, textBox2.Text, textBox3.Text, horas, años);
+            listBox1.Items.Add(amateur.MostrarAmat());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int cedu = Convert.ToInt32(textBox6.Text);
+            int horas = Convert.ToInt32(textBox9.Text);
+            int sueldo = Convert.ToInt32(textBox10.Text);
+            Class1.Profesional profesional = new Class1.Profesional(cedu, textBox7.Text, textBox8.Text, horas, sueldo);
+            listBox2.Items.Add(profesional.MostrarProf());
+        }
+    }
+}
